f6a7d6bac8df1130ee2b14bd93fe1d51c220f282
Mathieu Balesdent <mathieubalesdent@gmail.com>
2026-08-03 16:02:11 +0200

    fix override AKalgorithm

--- lib/src/Uncertainty/Algorithm/Simulation/openturns/ActiveLearningReliabilityAlgorithm.hxx
+++ lib/src/Uncertainty/Algorithm/Simulation/openturns/ActiveLearningReliabilityAlgorithm.hxx
@@ -155,24 +155,24 @@ protected:
 private :
 
   /** Maximum sample size accessor */
-  void setMaximumOuterSampling(const UnsignedInteger maximumOuterSampling);
-  UnsignedInteger getMaximumOuterSampling() const;
+  void setMaximumOuterSampling(const UnsignedInteger maximumOuterSampling) override;
+  UnsignedInteger getMaximumOuterSampling() const override;
 
   /** Maximum coefficient of variation accessor */
-  void setMaximumCoefficientOfVariation(const Scalar maximumCoefficientOfVariation);
-  Scalar getMaximumCoefficientOfVariation() const;
+  void setMaximumCoefficientOfVariation(const Scalar maximumCoefficientOfVariation) override;
+  Scalar getMaximumCoefficientOfVariation() const override;
   
   /** Convergence strategy accessor */
-  void setConvergenceStrategy(const HistoryStrategy & convergenceStrategy);
-  HistoryStrategy getConvergenceStrategy() const;
+  void setConvergenceStrategy(const HistoryStrategy & convergenceStrategy) override;
+  HistoryStrategy getConvergenceStrategy() const override;
   
   /** Maximum standard deviation accessor */
-  void setMaximumStandardDeviation(const Scalar maximumStandardDeviation);
-  Scalar getMaximumStandardDeviation() const;
+  void setMaximumStandardDeviation(const Scalar maximumStandardDeviation) override;
+  Scalar getMaximumStandardDeviation() const override;
 
   /** Block size accessor */
-  virtual void setBlockSize(const UnsignedInteger blockSize);
-  UnsignedInteger getBlockSize() const;
+  virtual void setBlockSize(const UnsignedInteger blockSize) override;
+  UnsignedInteger getBlockSize() const override;
   
 } ; /* class ActiveLearningReliabilityAlgorithm */
 
